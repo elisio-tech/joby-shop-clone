c59b4b8264aea199f23ff254bf49c78f55ab1399
Elisio Augusto <elisiouiux@gmail.com>
2026-03-26 11:21:05 +0100

    fix: page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -16,7 +16,7 @@ export default function Page({ params }: PageProps) {
 
   if (!product) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen ">
         <span className="text-6xl font-bold text-zinc-200">404</span>
         <p>Product not found!</p>
       </div>
@@ -24,52 +24,13 @@ export default function Page({ params }: PageProps) {
   }
 
   return (
-    <section className="mx-12 pt-32 grid grid-cols-1 lg:grid-cols-2 gap-12">
-      <div className="relative aspect-square bg-zinc-100 rounded-3xl overflow-hidden">
-        <Image
-          src={product.image}
-          alt={product.name}
-          fill
-          className="object-cover"
-        />
-      </div>
-
-      <div className="flex flex-col">
-        <h1 className="text-4xl font-bold mb-2">{product.name}</h1>
-        <span className="text-2xl text-blue-600 mb-8">{product.price}</span>
-
-        <div>
-          <p className="font-bold mb-4">Size</p>
-          <div className="flex gap-3">
-            {product.sizes.map((size) => (
-              <p key={size}>{size}</p>
-            ))}
-          </div>
-        </div>
-
-        <div className="flex gap-4 pt-8">
-          <div className="flex items-center gap-2 border border-zinc-400 rounded-4xl px-2">
-            <button
-              onClick={() => setQty(Math.max(1, qty - 1))}
-              disabled={qty === 1}
-              className={`px-3 py-1 text-2xl cursor-pointer ${qty === 1 ? "text-zinc-400" : "text-zinc-600"}`}
-            >
-              -
-            </button>
-
-            <span className="min-w-5 text-center px-4">{qty}</span>
-
-            <button
-              onClick={() => setQty(qty + 1)}
-              className="px-3 py-1 text-2xl cursor-pointer "
-            >
-              +
-            </button>
-          </div>
-
-          <button className="w-full py-4 bg-blue-500 text-white rounded-full cursor-pointer">
-            Add to Cart
-          </button>
+    <section className="mx-12">
+      <div className="pt-18 flex justify-center gap-14 h-screen">
+        <div className="bg-zinc-400 ">foto</div>
+        <div className="max-w-max">
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+          <button>Add to card</button>
         </div>
       </div>
     </section>
